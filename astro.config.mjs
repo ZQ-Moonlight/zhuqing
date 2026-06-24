86ffc3a92d56d8838eb2838c3f5d62a9d23a4757
Zhu Qing <zq2637597832@gmail.com>
2026-06-24 18:44:51 +0800

    尝试上传cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 
+const site = process.env.SITE_URL ?? 'https://zhuqing.tech';
+const base = process.env.BASE_PATH ?? '/';
+
 export default defineConfig({
-  site: 'https://zhuqing.tech',
+  site,
+  base,
 });
